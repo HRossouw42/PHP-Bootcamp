2c45b07af6a10a2dc9dca5a844bd3382cf4efd39
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-10-04 14:35:36 +0200

    ex 00

--- Day02/ex00/another_world.php
+++ Day02/ex00/another_world.php
@@ -1,8 +1,8 @@
-#!/usr/bin/php
+#!/usr/bin/php 
 <?php
-	if ($argc == 2) 
+	if ($argc > 1) 
 	{
-        $pieces = array_filter(explode(' ', $argv[1]));
+		$pieces = array_filter(explode(' ', $argv[1]));
         $final = "";
         foreach($pieces as $string)
             $final .= $string . " ";
