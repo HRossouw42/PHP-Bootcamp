a298b37871375b9b8203854a556f8a93c69736c8
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-10-05 17:00:51 +0200

    ex03

--- Day03/ex03/cookie_crisp.php
+++ Day03/ex03/cookie_crisp.php
@@ -1,1 +1,23 @@
-hell worldo
+<?php
+$input = $_GET;
+
+foreach($input as $key => $value)
+{
+	if ($key == 'action' && $value == 'set')
+	{
+		setcookie($input['name'], $input['value']);
+		break;
+	}
+	else if ($key == 'action' && $value == 'get')
+	{
+		echo ($_COOKIE[$input['name']]."\n");
+		break;
+	}
+	else if ($key == 'action' && $value == 'del')
+	{
+		setcookie($input['name'], '', 1);
+		setcookie($input['value'], '', 1);
+		break;
+	}
+}
+?>
