f528cc29c086dfd73f8beccb519253d0755b500b
Harmun ROSSOUW <hrossouw@c2r5s1.wethinkcode.co.za>
2018-10-11 15:07:48 +0200

    ex00 added

--- Day07/ex04/Jaime.class.php
+++ Day07/ex04/Jaime.class.php
@@ -5,11 +5,11 @@
         public function sleepWith($object)
         {
             if ($object instanceof Tyrion)
-                print("Not even if I'm drunk ! " . PHP_EOL);
+                echo("Not even if I'm drunk ! " . PHP_EOL);
             else if ($object instanceof Sansa)
-                print("Let's do this." . PHP_EOL);
+                echo("Let's do this." . PHP_EOL);
             else if ($object instanceof Cersei)
-                print("With pleasure, but only in a tower in Winterfell, then." . PHP_EOL);
+                echo("With pleasure, but only in a tower in Winterfell, then." . PHP_EOL);
         }
     }
 ?>
--- Day07/ex04/Tyrion.class.php
+++ Day07/ex04/Tyrion.class.php
@@ -6,9 +6,9 @@
         public function sleepWith($object)
         {
             if ($object instanceof Jaime || $object instanceof Cersei)
-                print("Not even if I'm drunk !" . PHP_EOL);
+                echo("Not even if I'm drunk !" . PHP_EOL);
             else if ($object instanceof Sansa)
-                print("Let's do this." . PHP_EOL);
+                echo("Let's do this." . PHP_EOL);
         }
     }
 ?>
